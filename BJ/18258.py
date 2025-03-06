59a49e7ef7e64b7d7d760546363d41865e7aac02
kminh-wow <kminh9635@daum.net>
2025-03-06 17:11:00 +0900

    123

---
+++ BJ/18258.py
@@ -0,0 +1,54 @@
+que = []
+
+
+
+def push(x):
+    que.append(x)
+    
+def pop():
+    if len(que) == 0:
+        print('-1')
+    else:
+        print(que[0])
+        que.remove[0]
+        
+def size():
+    print(len(que))
+    
+def empty():
+    if len(que) == 0:
+        print('1')
+    else:
+        print('0')
+
+def front():
+    if len(que) == 0:
+        print('-1')
+    else:
+        print(que[0])
+
+def back():
+    if len(que) == 0:
+        print('-1')
+    else:
+        print(que[-1])
+        
+
+n = int(input())
+
+for i in range(n):
+    x = list(input().split())
+    if x[0] == 'push':
+        push(int(x[1]))
+    elif x[0] == 'front':
+        front()
+    elif x[0] == 'pop':
+        pop()
+    elif x[0] == 'size':
+        size()
+    elif x[0] == 'empty':
+        empty()
+    elif x[0] == 'back':
+        back()
+    
+    
---
+++ BJ/2941.py
@@ -0,0 +1,22 @@
+def custom_replace(s, old, new):
+    result = ""
+    i = 0
+    old_len = len(old)
+    
+    while i < len(s):
+        # old 문자열이 현재 위치부터 일치하면 new로 변경
+        if s[i:i+old_len] == old:
+            result += new
+            i += old_len  # old의 길이만큼 건너뛰기
+        else:
+            result += s[i]
+            i += 1  # 한 글자씩 이동
+
+    return result
+
+croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+word = input()
+
+for i in croatia :
+    word = word.custom_replace(word,i, '*')  # input 변수와 동일한 이름의 변수
+print(len(word))
